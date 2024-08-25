e72c22104475a3ddf14a07229b398718e41181a4
KC1064 <kiron08072003@gmail.com>
2024-08-25 23:04:18 +0530

    Completed the Navbar and Sidebar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
+import React from "react";
+import Navbar from "./Components/Navbar";
+import SideBar from "./Components/SideBar";
+
 const App = () => {
   return (
-    <div className='bg-[#F5F8FC]'>
+    <div className="bg-[#F5F8FC]">
       <Navbar />
+      <SideBar />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ Frontend/src/Components/SideBar.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { LuPencil } from "react-icons/lu";
+import { MdInbox } from "react-icons/md";
+import { MdOutlineStarBorder } from "react-icons/md";
+import { MdOutlineWatchLater } from "react-icons/md";
+import { TbSend2 } from "react-icons/tb";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { MdOutlineDrafts } from "react-icons/md";
+
+const SideBar = () => {
+  const sidebar = [
+    {
+      icon: <MdInbox size={"20px"} />,
+      text: "Inbox",
+    },
+    {
+      icon: <MdOutlineStarBorder size={"20px"} />,
+      text: "Starred",
+    },
+    {
+      icon: <MdOutlineWatchLater size={"20px"} />,
+      text: "Snoozed",
+    },
+    {
+      icon: <TbSend2 size={"20px"} />,
+      text: "Sent",
+    },
+    {
+      icon: <MdOutlineDrafts size={"20px"} />,
+      text: "Draft",
+    },
+    {
+      icon: <MdOutlineKeyboardArrowDown size={"20px"} />,
+      text: "More",
+    },
+  ];
+
+  return (
+    <div className="w-[240px] h-full pt-3">
+      <div className="w-[140px] h-[55px] bg-[#C2E7FF] flex justify-center items-center rounded-2xl gap-2 ml-3 hover:scale-105 hover:shadow-lg transition-all duration-200 ease-in-out">
+        <button className="text-lg font-roboto flex justify-center items-center gap-3">
+          <LuPencil size={"20px"} /> <span>Compose</span>
+        </button>
+      </div>
+      <div className="pt-3 flex flex-col gap-2">
+        {sidebar.map((item, index) => {
+          return (
+            <div
+              className="w-full flex justify-start items-center gap-4 hover:bg-gray-200 hover:rounded-r-full pl-7 py-1"
+              key={index}
+            >
+              {item.icon}
+              <p className="text-[16px] font-roboto font-[500]">{item.text}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
